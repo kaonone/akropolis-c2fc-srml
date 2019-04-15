1cb6e315d179e8c1ccfbebd71dbd8de7108c0b6a
Alex Koz <a-k@akropolis.io>
2019-04-15 19:07:18 +0300

    migration to latest substrate

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -1,14 +1,16 @@
-use primitives::{Ed25519AuthorityId, ed25519};
+use primitives::{ed25519, sr25519, Pair};
 use akropolis_c2fc_runtime::{
 	AccountId, GenesisConfig, ConsensusConfig, TimestampConfig, BalancesConfig,
-	SudoConfig, IndicesConfig
+	SudoConfig, IndicesConfig,
 };
 use substrate_service;
+
+use ed25519::Public as AuthorityId;
 
 // Note this is the URL for the telemetry server
 //const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 
-/// Specialised `ChainSpec`. This is a specialisation of the general Substrate ChainSpec type.
+/// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = substrate_service::ChainSpec<GenesisConfig>;
 
 /// The chain specification option. This is expected to come in from the CLI and
@@ -20,8 +22,20 @@ pub enum Alternative {
 	Development,
 	/// Whatever the current runtime is, with simple Alice/Bob auths.
 	LocalTestnet,
-	/// Custom
+
 	Akropolis,
+}
+
+fn authority_key(s: &str) -> AuthorityId {
+	ed25519::Pair::from_string(&format!("//{}", s), None)
+		.expect("static values are valid; qed")
+		.public()
+}
+
+fn account_key(s: &str) -> AccountId {
+	sr25519::Pair::from_string(&format!("//{}", s), None)
+		.expect("static values are valid; qed")
+		.public()
 }
 
 impl Alternative {
@@ -32,11 +46,11 @@ impl Alternative {
 				"Development",
 				"dev",
 				|| testnet_genesis(vec![
-					ed25519::Pair::from_seed(b"Alice                           ").public().into(),
+					authority_key("Alice")
 				], vec![
-					ed25519::Pair::from_seed(b"Alice                           ").public().0.into(),
+					account_key("Alice")
 				],
-					ed25519::Pair::from_seed(b"Alice                           ").public().0.into()
+					account_key("Alice")
 				),
 				vec![],
 				None,
@@ -48,17 +62,17 @@ impl Alternative {
 				"Local Testnet",
 				"local_testnet",
 				|| testnet_genesis(vec![
-					ed25519::Pair::from_seed(b"Alice                           ").public().into(),
-					ed25519::Pair::from_seed(b"Bob                             ").public().into(),
+					authority_key("Alice"),
+					authority_key("Bob"),
 				], vec![
-					ed25519::Pair::from_seed(b"Alice                           ").public().0.into(),
-					ed25519::Pair::from_seed(b"Bob                             ").public().0.into(),
-					ed25519::Pair::from_seed(b"Charlie                         ").public().0.into(),
-					ed25519::Pair::from_seed(b"Dave                            ").public().0.into(),
-					ed25519::Pair::from_seed(b"Eve                             ").public().0.into(),
-					ed25519::Pair::from_seed(b"Ferdie                          ").public().0.into(),
+					account_key("Alice"),
+					account_key("Bob"),
+					account_key("Charlie"),
+					account_key("Dave"),
+					account_key("Eve"),
+					account_key("Ferdie"),
 				],
-					ed25519::Pair::from_seed(b"Alice                           ").public().0.into()
+					account_key("Alice"),
 				),
 				vec![],
 				None,
@@ -71,12 +85,16 @@ impl Alternative {
 				"Akropolis",
 				"akropolis",
 				|| {
-					testnet_genesis( vec![
-							ed25519::Pair::from_seed(b"Alice                           ").public().into(),
-						], vec![
-							ed25519::Pair::from_seed(b"Alice                           ").public().0.into(),
+					testnet_genesis(
+						vec![ authority_key("Alice"), authority_key("Bob"), ],
+						vec![ account_key("Alice"),
+							   account_key("Bob"),
+							   account_key("Charlie"),
+							   account_key("Dave"),
+							   account_key("Eve"),
+							   account_key("Ferdie"),
 						],
-						ed25519::Pair::from_seed(b"Alice                           ").public().0.into(),
+						account_key("Alice"),
 					)
 				},
 				vec![],
@@ -85,6 +103,7 @@ impl Alternative {
 				None,
 				None,
 			),
+
 		})
 	}
 
@@ -98,15 +117,15 @@ impl Alternative {
 	}
 }
 
-fn testnet_genesis(initial_authorities: Vec<Ed25519AuthorityId>, endowed_accounts: Vec<AccountId>, root_key: AccountId) -> GenesisConfig {
+fn testnet_genesis(initial_authorities: Vec<AuthorityId>, endowed_accounts: Vec<AccountId>, root_key: AccountId) -> GenesisConfig {
 	GenesisConfig {
 		consensus: Some(ConsensusConfig {
-			code: include_bytes!("../runtime/wasm/target/wasm32-unknown-unknown/release/akropolis_c2fc_runtime.compact.wasm").to_vec(),
+			code: include_bytes!("../runtime/wasm/target/wasm32-unknown-unknown/release/akropolis_c2fc_runtime_wasm.compact.wasm").to_vec(),
 			authorities: initial_authorities.clone(),
 		}),
 		system: None,
 		timestamp: Some(TimestampConfig {
-			period: 5,					// 5 second block time.
+			minimum_period: 15,
 		}),
 		indices: Some(IndicesConfig {
 			ids: endowed_accounts.clone(),
@@ -117,7 +136,8 @@ fn testnet_genesis(initial_authorities: Vec<Ed25519AuthorityId>, endowed_account
 			existential_deposit: 500,
 			transfer_fee: 0,
 			creation_fee: 0,
-			balances: endowed_accounts.iter().map(|&k|(k, (1 << 60))).collect(),
+			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
+			vesting: vec![],
 		}),
 		sudo: Some(SudoConfig {
 			key: root_key,
--- src/cli.rs
+++ src/cli.rs
@@ -1,31 +1,47 @@
-use service;
+use crate::service;
 use futures::{future, Future, sync::oneshot};
 use std::cell::RefCell;
 use tokio::runtime::Runtime;
 pub use substrate_cli::{VersionInfo, IntoExit, error};
 use substrate_cli::{informant, parse_and_execute, NoCustom};
 use substrate_service::{ServiceFactory, Roles as ServiceRoles};
-use chain_spec;
+use crate::chain_spec;
 use std::ops::Deref;
+use log::info;
 
 /// Parse command line arguments into service configuration.
-pub fn run<I, T, E>(args: I, exit: E, version: VersionInfo) -> error::Result<()> where
-	I: IntoIterator<Item = T>,
-	T: Into<std::ffi::OsString> + Clone,
-	E: IntoExit,
-{
+pub fn run<I, T, E>(args: I, exit: E, version: VersionInfo) -> error::Result<()>
+	where I: IntoIterator<Item = T>,
+	      T: Into<std::ffi::OsString> + Clone,
+	      E: IntoExit {
 	parse_and_execute::<service::Factory, NoCustom, NoCustom, _, _, _, _, _>(
-		load_spec, &version, "substrate-node", args, exit,
-	 	|exit, _custom_args, config| {
-			info!("{}", version.name);
-			info!("  version {}", config.full_version());
-			info!("  by Parity Technologies, 2017-2019");
-			info!("Chain specification: {}", config.chain_spec.name());
-			info!("Node name: {}", config.name);
-			info!("Roles: {:?}", config.roles);
-			let runtime = Runtime::new().map_err(|e| format!("{:?}", e))?;
-			let executor = runtime.executor();
-			match config.roles {
+	                                                                         load_spec,
+	                                                                         &version,
+	                                                                         "substrate-node",
+	                                                                         args,
+	                                                                         exit,
+	                                                                         |exit, _custom_args, config| {
+		                                                                         info!("{}", version.name);
+		                                                                         info!(
+		                                                                               "  version {}",
+		                                                                               config.full_version()
+		);
+		                                                                         info!(
+		                                                                               "  by {}, 2017, 2018",
+		                                                                               version.author
+		);
+		                                                                         info!(
+		                                                                               "Chain specification: {}",
+		                                                                               config.chain_spec.name()
+		);
+		                                                                         info!("Node name: {}", config.name);
+		                                                                         info!("Roles: {:?}", config.roles);
+		                                                                         let runtime =
+			                                                                         Runtime::new().map_err(|e| {
+				                                                                                       format!("{:?}", e)
+				                                                                                      })?;
+		                                                                         let executor = runtime.executor();
+		                                                                         match config.roles {
 				ServiceRoles::LIGHT => run_until_exit(
 					runtime,
 				 	service::Factory::new_light(config, executor).map_err(|e| format!("{:?}", e))?,
@@ -37,27 +53,22 @@ pub fn run<I, T, E>(args: I, exit: E, version: VersionInfo) -> error::Result<()>
 					exit
 				),
 			}.map_err(|e| format!("{:?}", e))
-		}
-	).map_err(Into::into).map(|_| ())
+		                                                                        },
+	).map_err(Into::into)
+	.map(|_| ())
 }
 
 fn load_spec(id: &str) -> Result<Option<chain_spec::ChainSpec>, String> {
 	Ok(match chain_spec::Alternative::from(id) {
-		Some(spec) => Some(spec.load()?),
-		None => None,
-	})
+		   Some(spec) => Some(spec.load()?),
+	     None => None,
+	   })
 }
 
-fn run_until_exit<T, C, E>(
-	mut runtime: Runtime,
-	service: T,
-	e: E,
-) -> error::Result<()>
-	where
-		T: Deref<Target=substrate_service::Service<C>>,
-		C: substrate_service::Components,
-		E: IntoExit,
-{
+fn run_until_exit<T, C, E>(mut runtime: Runtime, service: T, e: E) -> error::Result<()>
+	where T: Deref<Target = substrate_service::Service<C>>,
+	      C: substrate_service::Components,
+	      E: IntoExit {
 	let (exit_send, exit) = exit_future::signal();
 
 	let executor = runtime.executor();
@@ -65,6 +76,11 @@ fn run_until_exit<T, C, E>(
 
 	let _ = runtime.block_on(e.into_exit());
 	exit_send.fire();
+
+	// we eagerly drop the service so that the internal exit future is fired,
+	// but we need to keep holding a reference to the global telemetry guard
+	let _telemetry = service.telemetry();
+	drop(service);
 	Ok(())
 }
 
@@ -78,7 +94,10 @@ impl IntoExit for Exit {
 
 		let exit_send_cell = RefCell::new(Some(exit_send));
 		ctrlc::set_handler(move || {
-			if let Some(exit_send) = exit_send_cell.try_borrow_mut().expect("signal handler not reentrant; qed").take() {
+			if let Some(exit_send) = exit_send_cell.try_borrow_mut()
+			                                       .expect("signal handler not reentrant; qed")
+			                                       .take()
+			{
 				exit_send.send(()).expect("Error sending exit notification");
 			}
 		}).expect("Error setting Ctrl-C handler");
